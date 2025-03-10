d3e13789cca5426aefdd365d289ddda469d9278b
Smagin Maxim Alexeevich <maxprogger@mail.ru>
2025-03-10 11:43:19 +0700

    Improv the output of the rating of employees

--- src/main/java/ru/isands/test/estore/dto/PurchaseAmountByEmployeeDTO.java
+++ src/main/java/ru/isands/test/estore/dto/PurchaseAmountByEmployeeDTO.java
@@ -4,17 +4,48 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
 @Getter
 @Setter
 public class PurchaseAmountByEmployeeDTO {
-    public PurchaseAmountByEmployeeDTO(Long employeeId, Double totalAmount) {
+    public PurchaseAmountByEmployeeDTO(Long employeeId, Double totalAmount, String lastName, String firstName, String patronymic, LocalDate birthDate, Long positionId, Long shopId, Boolean gender) {
         setEmployeeId(employeeId);
         setTotalAmount(totalAmount);
+        setLastName(lastName);
+        setFirstName(firstName);
+        setPatronymic(patronymic);
+        setBirthDate(birthDate);
+        setPositionId(positionId);
+        setShopId(shopId);
+        setGender(gender);
     }
 
     @NonNull
-    Long employeeId;
+    private Long employeeId;
 
     @NonNull
-    Double totalAmount;
+    private Double totalAmount;
+
+    @NonNull
+    private String lastName;
+
+    @NonNull
+    private String firstName;
+
+    @NonNull
+    private String patronymic;
+
+    @NonNull
+    private LocalDate birthDate;
+
+    @NonNull
+    private Long positionId;
+
+    @NotNull
+    private Long shopId;
+
+    @NotNull
+    private boolean gender;
 }
--- src/main/java/ru/isands/test/estore/dto/PurchaseCountByEmployeeDTO.java
+++ src/main/java/ru/isands/test/estore/dto/PurchaseCountByEmployeeDTO.java
@@ -4,17 +4,48 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
 @Getter
 @Setter
 public class PurchaseCountByEmployeeDTO {
-    public PurchaseCountByEmployeeDTO(Long employeeId, Long totalCount) {
+    public PurchaseCountByEmployeeDTO(Long employeeId, Long totalCount, String lastName, String firstName, String patronymic, LocalDate birthDate, Long positionId, Long shopId, Boolean gender) {
         setEmployeeId(employeeId);
         setTotalCount(totalCount);
+        setLastName(lastName);
+        setFirstName(firstName);
+        setPatronymic(patronymic);
+        setBirthDate(birthDate);
+        setPositionId(positionId);
+        setShopId(shopId);
+        setGender(gender);
     }
 
     @NonNull
-    Long employeeId;
+    private Long employeeId;
 
     @NonNull
-    Long totalCount;
+    private Long totalCount;
+
+    @NonNull
+    private String lastName;
+
+    @NonNull
+    private String firstName;
+
+    @NonNull
+    private String patronymic;
+
+    @NonNull
+    private LocalDate birthDate;
+
+    @NonNull
+    private Long positionId;
+
+    @NotNull
+    private Long shopId;
+
+    @NotNull
+    private boolean gender;
 }
